31909b8cdbb97aa1f07439d9930ba01827382bf6
daw21-08 <5709111@alu.murciaeduca.es>
2023-10-18 20:58:26 +0200

    paso 93

--- app/controllers/CartController.php
+++ app/controllers/CartController.php
@@ -8,9 +8,27 @@ class CartController extends Controller
     {
         $this->model = $this->model('Cart');
     }
+    public function index($errors = [])
+    {
+        $session = new Session();
+
+        if ($session->getLogin()) {
+echo 'asdf';
+
+        } else {
+            header('location:' . ROOT);
+        }
+    }
 
     public function addProduct($product_id, $user_id)
     {
-        var_dump($product_id, $user_id);
+        $errors = [];
+
+        if ($this->model->verifyProduct($product_id, $user_id) == false) {
+            if ($this->model->addProduct($product_id, $user_id) == false) {
+                array_push($errors, 'Error al insertar el producto en el carrito');
+            }
+        }
+        $this->index($errors);
     }
 }
