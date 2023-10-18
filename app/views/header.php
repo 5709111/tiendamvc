aeec1a9603d2301dcb314ca72ef983f72396bce2
daw21-08 <5709111@alu.murciaeduca.es>
2023-10-18 20:32:24 +0200

    paso 92

--- app/views/header.php
+++ app/views/header.php
@@ -8,7 +8,7 @@
     <title><?= $data['title'] ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-
+    <script src="https://kit.fontawesome.com/aff61fee40.js" crossorigin="anonymous"></script>
 </head>
 <body>
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
@@ -38,7 +38,9 @@
                     <form action="<?= ROOT ?>search/products" class="d-flex form-inline" method="POST">
                         <input type="text" name="search" id="search" class="form-control"
                                size="20" placeholder="¿producto?" required>
-                        <input type="submit" class="btn btn-light" value="buscar">
+
+                        <button type="submit" class="btn btn-light" value="buscar">  <i class="fa-solid fa-magnifying-glass"></i>
+                            </button>
                     </form>
                 </li>
                 <li class="nav-item">
